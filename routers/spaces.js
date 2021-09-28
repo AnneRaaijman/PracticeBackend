7894ada8740c6648bf27ad547f6b5053267ad255
Anne Raaijman <anneraaijman@hotmail.com>
2021-09-28 13:26:26 +0200

    added space details and story ordering

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const story = require("../models").story;
 
 const router = new Router();
 
@@ -7,6 +8,16 @@ router.get("/", async (req, res) => {
   try {
     const spaces = await Space.findAll();
     res.send(spaces);
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const spaceId = parseFloat(req.params.id);
+    const spaceDetails = await Space.findByPk(spaceId, { include: [story] });
+    res.send(spaceDetails);
   } catch (e) {
     console.log(e.message);
   }
--- seeders/20210927101953-stories.js
+++ seeders/20210927101953-stories.js
@@ -8,7 +8,7 @@ module.exports = {
         {
           name: "what a story",
           content: "some real exciting stuff",
-          imageurl: null,
+          imageurl: "https://pbs.twimg.com/media/ETptP4cX0AEP8jl?format=jpg&name=4096x4096"
           spaceId: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -16,7 +16,7 @@ module.exports = {
         {
           name: "what a story 2",
           content: "some real exciting stuff 2",
-          imageurl: null,
+          imageurl: "https://eatliver.b-cdn.net/wp-content/uploads/2018/04/hilarious-cartoon1.png",
           spaceId: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
